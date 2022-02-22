622dc8dda49b2fe59ec4d653aa3a39dc08c1a280
wrac4242 <wrac4242@gmail.com>
2022-02-22 21:52:31 +0000

    rust format

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,23 @@
 pub struct Hashtable {
-
+    bucket_size: u32,
 }
 
 impl Hashtable {
-	pub fn new(bucket_size: u32) -> Hashtable {
-		Hashtable {
-		}
-	}
+    pub fn new(bucket_size: u32) -> Hashtable {
+        Hashtable {
+            bucket_size: bucket_size,
+        }
+    }
 
-	pub fn insert(&mut self, key: &str, value: &str) -> Result<(), ()> {
-		todo!();
-	}
-	
+    pub fn insert(&mut self, key: &str, value: &str) -> Result<(), ()> {
+        todo!();
+    }
 
-	pub fn get(&self, key: &str) -> Result<&str, ()> {
-		todo!();
-	}
+    pub fn get(&self, key: &str) -> Result<&str, ()> {
+        todo!();
+    }
 
-	pub fn remove(&mut self, key: &str) -> Result<(), ()> {
-		todo!();
-	}
+    pub fn remove(&mut self, key: &str) -> Result<(), ()> {
+        todo!();
+    }
 }
-
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2,84 +2,83 @@ use hashtables;
 
 #[test]
 fn inserting_getting() {
-	let mut ht = hashtables::Hashtable::new(1);
-	ht.insert("key", "value").unwrap();
+    let mut ht = hashtables::Hashtable::new(1);
+    ht.insert("key", "value").unwrap();
     assert_eq!(ht.get("key"), Ok("value"));
 }
 
 #[test]
 fn inserting_getting_multiple() {
-	let mut ht = hashtables::Hashtable::new(4);
-	ht.insert("key1", "value1").unwrap();
-	ht.insert("key2", "value2").unwrap();
-	ht.insert("key3", "value3").unwrap();
-	ht.insert("key4", "value4").unwrap();
-    
-	assert_eq!(ht.get("key1"), Ok("value1"));
-	assert_eq!(ht.get("key2"), Ok("value2"));
-	assert_eq!(ht.get("key3"), Ok("value3"));
-	assert_eq!(ht.get("key4"), Ok("value4"));
+    let mut ht = hashtables::Hashtable::new(4);
+    ht.insert("key1", "value1").unwrap();
+    ht.insert("key2", "value2").unwrap();
+    ht.insert("key3", "value3").unwrap();
+    ht.insert("key4", "value4").unwrap();
+
+    assert_eq!(ht.get("key1"), Ok("value1"));
+    assert_eq!(ht.get("key2"), Ok("value2"));
+    assert_eq!(ht.get("key3"), Ok("value3"));
+    assert_eq!(ht.get("key4"), Ok("value4"));
 }
 
 #[test]
 fn errors_insert_if_key_blank() {
-	let mut ht = hashtables::Hashtable::new(1);
-	assert_eq!(ht.insert("", "value4"), Err(()));
+    let mut ht = hashtables::Hashtable::new(1);
+    assert_eq!(ht.insert("", "value4"), Err(()));
 }
 
 #[test]
 fn errors_insert_not_set_key() {
-	let mut ht = hashtables::Hashtable::new(1);
-	assert_eq!(ht.insert("key", ""), Err(()));
+    let mut ht = hashtables::Hashtable::new(1);
+    assert_eq!(ht.insert("key", ""), Err(()));
 }
 
 #[test]
 fn inserting_removing() {
-	let mut ht = hashtables::Hashtable::new(1);
-	ht.insert("key", "value").unwrap();
-	ht.remove("key").unwrap();
-    assert_eq!(ht.get("key"), Err(()));
+    let mut ht = hashtables::Hashtable::new(1);
+    ht.insert("key", "value").unwrap();
+    ht.remove("key").unwrap();
+	assert_eq!(ht.get("key"), Err(()));
 }
-
 
 #[test]
 fn inserting_getting_collision() {
-	let mut ht = hashtables::Hashtable::new(3);
-	ht.insert("key1", "value1").unwrap();
-	ht.insert("key2", "value2").unwrap();
-	ht.insert("key3", "value3").unwrap();
-	ht.insert("key4", "value4").unwrap();
-    
-	assert_eq!(ht.get("key1"), Ok("value1"));
-	assert_eq!(ht.get("key2"), Ok("value2"));
-	assert_eq!(ht.get("key3"), Ok("value3"));
-	assert_eq!(ht.get("key4"), Ok("value4"));
+    let mut ht = hashtables::Hashtable::new(3);
+    ht.insert("key1", "value1").unwrap();
+    ht.insert("key2", "value2").unwrap();
+    ht.insert("key3", "value3").unwrap();
+    ht.insert("key4", "value4").unwrap();
+
+    assert_eq!(ht.get("key1"), Ok("value1"));
+    assert_eq!(ht.get("key2"), Ok("value2"));
+    assert_eq!(ht.get("key3"), Ok("value3"));
+    assert_eq!(ht.get("key4"), Ok("value4"));
 }
 
 #[test]
 fn inserting_removing_collision() {
-	let mut ht = hashtables::Hashtable::new(3);
-	ht.insert("key1", "value1").unwrap();
-	ht.insert("key2", "value2").unwrap();
-	ht.insert("key3", "value3").unwrap();
-	ht.insert("key4", "value4").unwrap();
+    let mut ht = hashtables::Hashtable::new(3);
+    ht.insert("key1", "value1").unwrap();
+    ht.insert("key2", "value2").unwrap();
+    ht.insert("key3", "value3").unwrap();
+    ht.insert("key4", "value4").unwrap();
 
-	ht.remove("key4").unwrap();
-    
-	assert_eq!(ht.get("key1"), Ok("value1"));
-	assert_eq!(ht.get("key2"), Ok("value2"));
-	assert_eq!(ht.get("key3"), Ok("value3"));
-	assert_eq!(ht.get("key4"), Err(()));
+    ht.remove("key4").unwrap();
+
+    assert_eq!(ht.get("key1"), Ok("value1"));
+    assert_eq!(ht.get("key2"), Ok("value2"));
+    assert_eq!(ht.get("key3"), Ok("value3"));
+    assert_eq!(ht.get("key4"), Err(()));
 }
 
 #[test]
 fn errors_remove_not_set_key() {
-	let mut ht = hashtables::Hashtable::new(1);
-	assert_eq!(ht.remove(""), Err(()));
+    let mut ht = hashtables::Hashtable::new(1);
+    assert_eq!(ht.remove(""), Err(()));
 }
 
 #[test]
 fn errors_remove_wrong_key() {
-	let mut ht = hashtables::Hashtable::new(1);
-	assert_eq!(ht.remove("key"), Err(()));
+    let mut ht = hashtables::Hashtable::new(1);
+    assert_eq!(ht.remove("key"), Err(()));
 }
